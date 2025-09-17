0612bfb5b054763232670a16eec6560084a0bd26
MuhammadAliZaidi <aghazaidi@yandex.com>
2025-09-17 22:36:28 +0500

    Update Task3.cpp

--- Lab3/Task3.cpp
+++ Lab3/Task3.cpp
@@ -1,1 +1,20 @@
-
+#include <iostream>
+using namespace std;
+class Calculator {
+private:
+    int x, y;
+public:
+    void setNumbers(int a, int b) {
+        x = a;
+        y = b;
+    }
+    int add() {
+        return x + y;
+    }
+};
+int main() {
+    Calculator calc;
+    calc.setNumbers(234, 2);
+    cout << "Sum: " << calc.add() << endl;
+    return 0;
+}
