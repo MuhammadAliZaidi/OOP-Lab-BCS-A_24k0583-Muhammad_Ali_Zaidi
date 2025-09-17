a9300b8c1891087f36e5d9a4260c3791d919366e
MuhammadAliZaidi <aghazaidi@yandex.com>
2025-09-17 22:33:32 +0500

    Update Task2.cpp

--- Lab3/Task2.cpp
+++ Lab3/Task2.cpp
@@ -1,1 +1,25 @@
-
+#include <iostream>
+using namespace std;
+class Student {
+public:
+    string name;
+private:    
+    int rollNumber;
+public:
+    void setRollNo(int x)
+    {
+        rollNumber = x;
+    }
+    int getRollNo()
+    {
+        return rollNumber;
+    }
+};
+int main() {
+    Student s;
+    s.name = "Ali Khan";
+    s.setRollNo(2025);
+    cout << "Name: " << s.name << endl;
+    cout << "Roll Number: " << s.getRollNo() << endl;
+    return 0;
+}
